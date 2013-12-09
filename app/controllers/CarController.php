d7cfad9336a111163cb95e4574099780a27c4801
Pedro Pereira <pedro@propertyguru.com.sg>
2013-12-09 12:31:14 +0800

    Car using master layout

--- app/controllers/CarController.php
+++ app/controllers/CarController.php
@@ -5,7 +5,7 @@ class CarController extends BaseController {
     public function getIndex($id)
     {
         $car = Car::find($id);
-        
+
         if (null === $car) {
             return View::make('404');
         }
