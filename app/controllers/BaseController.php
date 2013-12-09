09609d2f87ecc6bb1e749fee6b73efc3c2a819ff
Nico Hiort af Ornäs <nico@propertyguru.com.sg>
2013-12-09 14:01:46 +0800

    User login page tests.

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -1,6 +1,7 @@
 <?php
 
 class BaseController extends Controller {
+    
 	protected $layout = 'layouts.master';
 
 	/**
---
+++ app/tests/UserControllerTest.php
@@ -0,0 +1,40 @@
+<?php
+
+class UserControllerTest extends TestCase {
+
+    public function testShouldGetLoginRedirectIfLogged()
+    {
+        $user = new User;
+        $user->username = 'dummy';
+        $user->email = 'dummy@dummy.com';
+
+        Confide::shouldReceive('user')->once()->andReturn($user);
+
+        $this->action('GET', 'UserController@login');
+
+        $this->assertRedirectedTo('/');
+    }
+
+    public function testShouldGetLogin()
+    {
+        Confide::shouldReceive('user')
+            ->andReturn(null)
+            ->once();
+
+        Config::shouldReceive('get')
+            ->with('confide::login_form')
+            ->once()
+            ->andReturn('user.login');
+
+        View::shouldReceive('make')
+            ->with('user.login');
+
+        View::shouldReceive('make')
+            ->with('layouts.master');
+
+        $this->action('GET', 'UserController@login');
+
+        $this->assertViewHasAll(array());
+    }
+
+}
