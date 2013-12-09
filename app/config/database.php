a709faae9c8a4e8242ec9d528bbdcb28cf0a4a18
Nico Hiort af Ornäs <nico@propertyguru.com.sg>
2013-12-09 10:28:39 +0800

    Changed database config credentials.

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
-			'username'  => 'root',
-			'password'  => '',
+			'database'  => 'caravel',
+			'username'  => 'caravel',
+			'password'  => 'caravel',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
